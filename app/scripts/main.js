876b8e0e768df3dbfb7e561462ba55ea6a25cf04
Forest_Hub <anatolijk096@gmail.com>
2020-06-08 20:56:40 +0300

    render posts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,6 @@
 // 1. Получить все посты  по урлу http://localhost:3000/posts
 // 2. Отрисовать все посты в #app
+//3. При нажатии на кнопку выводился id поста в консоль
 
 //1. Получил данные всех постов
 const postsURL = "http://localhost:3000/posts";
@@ -9,4 +10,58 @@ function sendRequest(url) {
     return response.json();
   });
 }
-sendRequest(postsURL).then((data) => console.log(data));
+function initPostsList() {
+  sendRequest(postsURL).then(appendList);
+}
+
+function appendList(data) {
+  const listNode = creatList(data);
+  const app = document.getElementById("app");
+  app.append(listNode);
+}
+
+function creatList(postsData) {
+  //Функция которая возвращает div внутри которого будут лежать все посты.
+  const list = document.createElement("div");
+  postsData.forEach((post) => {
+    list.append(createPost(post));
+  });
+  return list;
+}
+
+function createPost({ id, img, title, description, link, tags }) {
+  const post = document.createElement("div");
+  post.classList.add("post-item");
+
+  const postImg = document.createElement("img");
+  postImg.src = img;
+
+  const postTitle = document.createElement("h2");
+  postTitle.classList.add("post-title");
+  postTitle.innerHTML = title;
+
+  const postTags = document.createElement("span");
+  postTags.classList.add("post-tags");
+  postTags.innerHTML = tags;
+
+  const postText = document.createElement("p");
+  postText.innerHTML = description;
+
+  const postLink = document.createElement("a");
+  postLink.href = link;
+  postLink.innerHTML = "Link";
+
+  const favoritBtn = document.createElement("button");
+  favoritBtn.innerHTML = "Add to favorite";
+  favoritBtn.classList.add("favorit-btn");
+
+  favoritBtn.addEventListener("click", () => {
+    console.log(id);
+  });
+
+  post.append(postImg, postTitle, postTags, postText, postLink, favoritBtn);
+
+  return post;
+}
+
+initPostsList();
