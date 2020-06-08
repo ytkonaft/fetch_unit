aabce87283e9d5e1252fba74f4869b3d2ec721ef
Forest_Hub <anatolijk096@gmail.com>
2020-06-08 14:10:17 +0300

    received data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,7 @@
 // 1. Получить все посты  по урлу http://localhost:3000/posts
 // 2. Отрисовать все посты в #app
 
+//1. Получил данные всех постов
 const postsURL = "http://localhost:3000/posts";
 
 function sendRequest(url) {
